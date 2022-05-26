3ed729740b8196613f846d330965370650f5a342
Lucas Sato <62661137+satoajato@users.noreply.github.com>
2022-05-26 19:51:17 -0300

    Add files via upload

---
+++ Calculadora IHC Ruim III/src/calculadora/CalculadoraRuimIII.java
@@ -0,0 +1,30 @@
+package calculadora;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sato1
+ */
+public class CalculadoraRuimIII extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLCalculadora.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
